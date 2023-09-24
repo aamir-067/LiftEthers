e5d5046f24981496d0108247ea2d7458b8aabb35
aamir <aamirkhan@engineer.com>
2023-09-24 12:45:30 +0500

    production build added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,9 @@ const Hero = () => {
         <section>
             <img src={banner} className='min-w-screen' alt="banner in which i mention that give me some ethers" />
 
-            <div className='w-11/12 md:w-9/12 border-3 border-black'>
-
+            <div className='w-11/12 md:w-9/12 border-3 border-dashed my-10 border-black'>
+                <h2>Donate Ethers</h2>
+                <p>please connect sepolia testnet to fetch data</p>
             </div>
 
         </section>
